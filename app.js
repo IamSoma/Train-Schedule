65f190d725a35d8bd2e7262512a39b8568b09670
Nick Grismore <nicksk83@aol.com>
2017-10-04 16:15:13 -0400

    added bootstrap and firebase to app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+  var config = {
+    apiKey: "AIzaSyAI5tfpB5yorMzUz-omr8C750OjgKHTe_Q",
+    authDomain: "trainschedule-57c42.firebaseapp.com",
+    databaseURL: "https://trainschedule-57c42.firebaseio.com",
+    projectId: "trainschedule-57c42",
+    storageBucket: "trainschedule-57c42.appspot.com",
+    messagingSenderId: "974399855907"
+  };
+  
+  firebase.initializeApp(config);
