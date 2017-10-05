e655cfeecde48dc01e54deb7a4f5249a4d489c41
Nick Grismore <nicksk83@aol.com>
2017-10-05 15:55:02 -0400

    added firebase to app.js and additional bootstrap comps

--- app.js
+++ app.js
@@ -8,5 +8,29 @@
   };
 
   firebase.initializeApp(config);
-  
-  moment().format();
+
+  var trainData = firebase.database();
+
+  $('#button').on('click', function(){
+  	var trainName = $('#trainName').val.trim();
+  	var destination = $('#destination').val.trim();
+  	var trainTime = moment($('#trainTime').val.trim(),'HH:mm').subtract(10,'years').format('X');
+  	var frequency = $('#frequency').val.trim();
+  	var newTrain = {
+  		name: trainName,
+  		destination: destination,
+  		time: trainTime,
+  		frequency: frequency 
+  	}
+  	trainData.ref().push(newTrain);
+  	alert('train added');
+
+  	$('#trainName').val('');
+  	$('#destination').val('');
+  	$('#trainTime').val('');
+  	$('#frequency').val('');
+
+  	return false;
+  })
+
+  //create a connection from 
