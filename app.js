38670de3cbd72aab24ada27d29ae58f6630424fa
Nick Grismore <nicksk83@aol.com>
2017-10-06 01:20:29 -0400

    logic updated on app.js

--- app.js
+++ app.js
@@ -38,17 +38,17 @@ $("#trainBtn").on("click", function(){
   	return false;
   })
 
-  // trainData.ref().on("child_added", function(snapshot){
-  //   var name = snapshot.val().name;
-  //   var destination = snapshot.val().destination;
-  //   var frequency = snapshot.val().frequency;
-  //   var firstTrain = snapshot.val().firstTrain;
+  trainData.ref().on("child_added", function(snapshot){
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var trainTime = snapshot.val().trainTime;
 
-  //   var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency;
-  //   var minutes = frequency - remainder;
-  //   var arrival = moment().add(minutes,"m").format("hh:mm A");
+    var remainder = moment().diff(moment.unix(trainTime), "minutes")%frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes,"m").format("hh:mm A");
 
-  //   $('#trainTable > tBody').append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"
-  //     +minutes+"</td></tr>");
-  // })
+    $('#trainTable > tBody').append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"
+      +minutes+"</td></tr>");
+  })
 
