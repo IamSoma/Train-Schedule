35f6e995ae0256fc7de8d3b2fdfe79c8d9d89f61
Nick Grismore <nicksk83@aol.com>
2017-10-05 22:28:20 -0400

    took out panels and added more jQuery and firebase code

--- app.js
+++ app.js
@@ -7,15 +7,20 @@
     messagingSenderId: "974399855907"
   };
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  var trainData = firebase.database();
 
-  $('#button').on('click', function(){
-  	var trainName = $('#trainName').val.trim();
-  	var destination = $('#destination').val.trim();
-  	var trainTime = moment($('#trainTime').val.trim(),'HH:mm').subtract(10,'years').format('X');
-  	var frequency = $('#frequency').val.trim();
+var trainData = firebase.database();
+
+trainData.ref().on("value" ,function(snapshot){
+  console.log(snapshot.val().something);
+})
+
+$("#trainBtn").on("click", function(){
+  	var trainName = $('#trainName').val().trim();
+  	var destination = $('#destination').val().trim();
+  	var trainTime = moment($('#trainTime').val().trim(),"HH:mm").subtract(10,'years').format('X');
+  	var frequency = $('#frequency').val().trim();
   	var newTrain = {
   		name: trainName,
   		destination: destination,
@@ -25,12 +30,25 @@
   	trainData.ref().push(newTrain);
   	alert('train added');
 
-  	$('#trainName').val('');
-  	$('#destination').val('');
-  	$('#trainTime').val('');
-  	$('#frequency').val('');
+  	$('#trainName').val("");
+  	$('#destination').val("");
+  	$('#trainTime').val("");
+  	$('#frequency').val("");
 
   	return false;
   })
 
-  //create a connection from 
+  // trainData.ref().on("child_added", function(snapshot){
+  //   var name = snapshot.val().name;
+  //   var destination = snapshot.val().destination;
+  //   var frequency = snapshot.val().frequency;
+  //   var firstTrain = snapshot.val().firstTrain;
+
+  //   var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency;
+  //   var minutes = frequency - remainder;
+  //   var arrival = moment().add(minutes,"m").format("hh:mm A");
+
+  //   $('#trainTable > tBody').append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"
+  //     +minutes+"</td></tr>");
+  // })
+
